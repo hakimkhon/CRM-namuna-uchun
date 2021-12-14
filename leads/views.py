ad0efbe8b578eeac94d5fde8c89d795f73e8ecac
hakimkhon <hakimkhon94@gmail.com>
2021-12-14 11:12:27 +0500

    16-dars

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-def home(requires):
-  return HttpResponse("Salom Dunyo")
+def home(request):
+  return render(request, "main.html")
     
