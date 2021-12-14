719671badfaad52781cb458292a287f4db2cc548
hakimkhon <hakimkhon94@gmail.com>
2021-12-14 17:17:57 +0500

    18-dars

--- leads/views.py
+++ leads/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 
 def home(request):
-  return render(request, "main.html")
+  context = {
+    "name": "Hakimkhon",
+    "surname": "Sharifxonov"
+  }
+  return render(request, "main.html", context)
     
