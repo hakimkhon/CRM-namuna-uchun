c1a42b5cb241bf1e2f630ee3671e24bbc5847218
hakimkhon <hakimkhon94@gmail.com>
2021-12-17 10:22:52 +0500

    29-dars

--- leads/views.py
+++ leads/views.py
@@ -4,15 +4,17 @@ from . import models
 
 def leads_list(request):
   leads = models.Lead.objects.all()
-  # agents = models.Agent.objects.all()
   context = {
     "leads": leads,
-    # "agents": agents
   }
   return render(request, "leads_list.html", context)
     
 def leads_detail(request, pk):
-  # print(pk)
   lead = get_object_or_404(models.Lead, id=pk)
-  # print(lead)
-  return render(request, 'details.html')
+  context = {
+    "lead": lead
+  }
+  return render(request, 'details.html', context)
+
+def create(request):
+  return render(request, 'create.html')
